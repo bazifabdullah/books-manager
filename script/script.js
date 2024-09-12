19bf733d74bc0e91cde9dd383082337f253d6369
Bazif Abdullah <bazifabdullah@gmail.com>
2024-09-12 23:58:45 +0500

    Create Book Cards Dynamically

--- script/script.js
+++ script/script.js
@@ -34,12 +34,42 @@ function showBookCards() {
     myLibrary.forEach((book, index) => createCard(book, index));
 }
 
+function createCard(book, index) {
+    const card = document.createElement('div');
+    const text = document.createElement('p');
+    const btnDiv = document.createElement('div');
+    const deleteBtn = document.createElement('button');
+    const readStatusBtn = document.createElement('button');
+
+    card.classList.add('single-card');
+    card.dataset.index = index;
+    text.classList.add('bookInfo');
+    btnDiv.classList.add('btnDiv');
+    deleteBtn.classList.add('delBtn');
+    readStatusBtn.classList.add('readBtn');
+    
+    text.textContent = `${book.title} by ${book.author}, Pages: ${book.pages}, ${book.readStatus}`;
+
+    bookCards.appendChild(card);
+    card.appendChild(text);
+    card.appendChild(btnDiv);
+    btnDiv.appendChild(deleteBtn);
+    btnDiv.appendChild(readStatusBtn);
+}
+
 function getInputValues() {
     const titleInput = document.querySelector('#input-title').value;
     const authorInput = document.querySelector('#input-author').value;
     const pagesInput = document.querySelector('#input-pages').value;
     const readCheck = document.querySelector('#read-Check').value;
-    return new book(titleInput, authorInput, pagesInput, readCheck)
+    let bookReadStatus;
+    if (readCheck === true) {
+        bookReadStatus = 'Already Read'
+    } else {
+        bookReadStatus = 'Not Read Yet'
+    }
+
+    return new book(titleInput, authorInput, pagesInput, bookReadStatus)
 }
 
 function addBookToLibrary() {
@@ -48,3 +78,14 @@ function addBookToLibrary() {
 }
 
 const myLibrary = [];
+
+inputSubmitButton.addEventListener('click', (event) => {
+
+    formElement.style.display = "none";
+    heroElement.style.display = "none";
+    event.preventDefault();
+    addBookToLibrary();
+    showBookCards();
+    formElement.reset();
+});
+      
